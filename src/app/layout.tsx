0eb0c57d20f9865910d8e35e1f07c390021d0624
guram12 <guram.shanidze.33@gmail.com>
2025-08-22 15:07:17 +0400

    feat(layout): add favicon link to the head section of the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/starmap_logo.svg" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AuthProvider>
           <Header />
